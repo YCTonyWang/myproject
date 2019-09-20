7a3f3ac650a89d11a9f5c78af3e3b86a83658770
tony_wang <YcTony0507>
2019-09-20 13:16:02 +0800

    Initial commit

---
+++ src/main/kotlin/wvt/org/Hello.kt
@@ -0,0 +1,15 @@
+package wvt.org
+
+fun main(args: Array<String>) {
+//    println("Hello kotlin!")
+//    Human().hello()
+    val h=Human()
+    h.hello()
+}
+
+class Human {
+    fun hello() {
+        println("Hello Kotlin!")
+    }
+
+}
